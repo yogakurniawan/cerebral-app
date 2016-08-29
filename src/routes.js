f8ebbcf1d1885fc9b4c988a2165e97147a5121ba
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-08-29 07:26:55 +0700

    chore(package) upgrade bootstrap sass loader to bootstrap loader

--- src/routes.js
+++ src/routes.js
@@ -35,24 +35,25 @@ export default (store) => {
    * Please keep routes in alphabetical order
    */
   return (
-    <Route path="/" component={App}>
-      { /* Home (main) route */ }
-      <IndexRoute component={Home}/>
+    <Route path="/" component={Login}>
+      <Route path="/app" component={App}>
+        { /* Home (main) route */ }
+        <IndexRoute component={Home}/>
 
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="chat" component={Chat}/>
-        <Route path="loginSuccess" component={LoginSuccess}/>
+        { /* Routes requiring login */ }
+        <Route onEnter={requireLogin}>
+          <Route path="chat" component={Chat}/>
+          <Route path="loginSuccess" component={LoginSuccess}/>
+        </Route>
+
+        { /* Routes */ }
+        <Route path="about" component={About}/>
+        <Route path="survey" component={Survey}/>
+        <Route path="widgets" component={Widgets}/>
+
+        { /* Catch all route */ }
+        <Route path="*" component={NotFound} status={404} />
       </Route>
-
-      { /* Routes */ }
-      <Route path="about" component={About}/>
-      <Route path="login" component={Login}/>
-      <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
-
-      { /* Catch all route */ }
-      <Route path="*" component={NotFound} status={404} />
     </Route>
   );
 };
