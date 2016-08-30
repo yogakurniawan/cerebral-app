fc1d61dc060df147e1b406f385b9afe363c450a1
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-08-30 07:20:37 +0700

    dev(feature) login page

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -26,22 +26,21 @@ export default class Login extends Component {
     return (
       <div className={styles.loginPage + ' container'}>
         <Helmet title="Login"/>
-        <h1>Login</h1>
         {!user &&
         <div>
           <form className="login-form" onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="login-username">Username</label>
-              <input type="text" ref="username" id="login-username" placeholder="Enter a username" className="form-control"/>
+            <h2>Login</h2>
+            <div className="input-group">
+              <span className="input-group-addon" id="username-addon"><i className={'fa fa-at ' + styles.faAt} /></span>
+              <input aria-describedby="username-addon" type="text" ref="username" id="login-username" placeholder="Username" className="form-control"/>
             </div>
-            <div className="form-group">
-              <label htmlFor="login-password">Password</label>
-              <input type="password" ref="password" id="login-password" placeholder="Password" className="form-control"/>
+            <div className="input-group">
+              <span className="input-group-addon" id="password-addon"><i className={'fa fa-lock ' + styles.faLock} /></span>
+              <input aria-describedby="password-addon" type="text" ref="password" id="login-password" placeholder="Password" className="form-control"/>
             </div>
             <button className="btn btn-success" onClick={this.handleSubmit}><i className="fa fa-sign-in"/>{' '}Log In
             </button>
           </form>
-          <p>This will "log you in" as this user, storing the username in the session of the API server.</p>
         </div>
         }
         {user &&
