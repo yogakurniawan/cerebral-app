aec186834d1a538a32b165f8bbdbcad09d1cb3a7
yogakurniawan <yoga.kwn@gmail.com>
2016-09-02 20:12:09 +0700

    dev(feature) login page

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -64,7 +64,7 @@ export default class App extends Component {
     return (
       <div className={styles.app}>
         <Helmet {...config.app.head}/>
-        <Navbar fixedTop>
+        {user && <Navbar fixedTop>
           <Navbar.Header>
             <Navbar.Brand>
               <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
@@ -110,19 +110,19 @@ export default class App extends Component {
               </NavItem>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
+        </Navbar>}
 
         <div className={styles.appContent}>
           {this.props.children}
         </div>
-        <InfoBar/>
+        {user && <InfoBar/>}
 
-        <div className="well text-center">
+        {user && <div className="well text-center">
           Have questions? Ask for help <a
           href="https://github.com/erikras/react-redux-universal-hot-example/issues"
           target="_blank">on Github</a> or in the <a
           href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
-        </div>
+        </div>}
       </div>
     );
   }
