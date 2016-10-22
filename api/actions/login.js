97c5257978a9981cf668ca7f2fb1ecec63b28b9f
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-10-22 20:23:37 +0700

    chore(project) delete unused files

--- api/actions/login.js
+++
@@ -1,8 +0,0 @@
-export default function login(req) {
-  const user = {
-    name: req.body.name,
-    password: req.body.password
-  };
-  req.session.user = user;
-  return Promise.resolve(user);
-}
