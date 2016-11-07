dfe6478e1e3e9a73097ad92a5c5a478afb606dec
Yoga Kurniawan <yoga.kwn@gmail.com>
2016-11-08 00:07:59 +0700

    dev(common) change logo

--- src/components/Registration/EmailVerified.js
+++ src/components/Registration/EmailVerified.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import Helmet from 'react-helmet';
 import { push } from 'react-router-redux';
 import {connect} from 'react-redux';
+import styles from 'containers/Login/Login.scss';
 
 @connect(undefined, { pushState: push })
 export default class RegistrationSuccess extends Component {
@@ -14,12 +15,11 @@ export default class RegistrationSuccess extends Component {
   }
 
   render() {
-    const styles = require('containers/Login/Login.scss');
     return (
       <div className={styles.loginPage + ' container'}>
         <Helmet title="Registration Success" />
         <div className="row">
-          <div className={styles.appLogo}><img src="https://storage.googleapis.com/cerebral/cerebral-app-logo.svg" /></div>
+          <div className={styles.appLogo}><img src="https://storage.googleapis.com/cerebral/cerebral-logo.png" /></div>
           <div className="alert alert-success text-center" role="alert">
             <p>Congratulations!</p>
             <p>Your email has been verified.</p>
